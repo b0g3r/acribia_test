fb978afa77a8889d9031a2e571425cd8e82dba3a
Boger <kotvberloge@gmail.com>
2018-10-18 23:49:14 +0300

    Reformat finder service

--- finder/main.py
+++ finder/main.py
@@ -1,31 +1,18 @@
 from asyncio import as_completed
 
-import aiodns
 from aiohttp import web
 
-from finder.subdomains import subdomains
-
-app = web.Application()
-resolver = aiodns.DNSResolver(loop=app.loop)
-
-
-async def async_check(host: str, sub_domain: str):
-    query = f'{sub_domain}.{host}'
-    try:
-        await resolver.query(query, 'A')
-    except (aiodns.error.DNSError, UnicodeError):
-        return None
-    return query
+from finder.app import app
+from finder.resolver import generate_check_set
 
 
 async def hello(request):
-    result = ""
-    tasks = [async_check('google.com', subdomain) for subdomain in subdomains]
-    for task in as_completed(tasks):
-        a = await task
-        if a:
-            result+=a+'\n'
-    return web.Response(text=result)
+    hosts = []
+    for check in as_completed(generate_check_set('google.com', 1)):
+        result = await check
+        if result:
+            hosts.append(result)
+    return web.Response(text='\n'.join(hosts))
 
 app.add_routes([web.get('/', hello)])
 web.run_app(app, port=10001)
---
+++ finder/resolver.py
@@ -0,0 +1,22 @@
+import itertools
+from typing import List, Iterable, Coroutine, Optional, Awaitable
+
+import aiodns
+
+from finder.app import resolver
+from finder.subdomains import subdomains
+
+
+async def async_check(domain: str) -> Optional[str]:
+    try:
+        await resolver.query(domain, 'A')
+    except (aiodns.error.DNSError, UnicodeError):
+        return None
+    return domain
+
+
+def generate_check_set(host: str, deep: int = 2) -> Iterable[Awaitable[Optional[str]]]:
+    return [
+        async_check(f"{'.'.join(r)}.{host}")
+        for r in itertools.product(subdomains, repeat=deep)
+    ]
